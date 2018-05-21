eb2f80d8878c0e949d1dd0cd1ec8cf18ee472866
Alexander Bobrov <al.bobrov.mail@gmail.com>
2018-05-21 17:35:35 +0500

    Added feil save arguments

--- app/jobs/cangaroo/job.rb
+++ app/jobs/cangaroo/job.rb
@@ -9,22 +9,27 @@ module Cangaroo
     end
 
     def source_connection
+      return unless arguments.first
       arguments.first.fetch(:source_connection)
     end
 
     def type
+      return unless arguments.first
       arguments.first.fetch(:type)
     end
 
     def request_params
+      return unless arguments.first
       arguments.first.fetch(:request_params, {})
     end
 
     def payload
+      return unless arguments.first
       arguments.first.fetch(:payload)
     end
 
     def vendor
+      return unless arguments.first
       arguments.first.fetch(:vendor, nil)
     end
   end
